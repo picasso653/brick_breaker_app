2205221cb72829e50635954a987f582fcea03199
Isaac Korankye Asante <xquisit31@gmail.com>
2023-05-07 22:04:26 +0000

    Daily code

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,6 +4,7 @@ import 'package:brick_breaker_app/ball.dart';
 import 'package:brick_breaker_app/cover_screen.dart';
 import 'package:brick_breaker_app/player.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -28,12 +29,30 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void moveLeft() {
+    setState(() {
+      playerX -= 0.2;
+    });
+  }
+
+  void moveRight() {
+    setState(() {
+      playerX += 0.2;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return RawKeyboardListener(
       focusNode: FocusNode(),
       autofocus: true,
-      onKey: (event) {},
+      onKey: (event) {
+        if (event.isKeyPressed(LogicalKeyboardKey.arrowLeft)) {
+          moveLeft();
+        } else if (event.isKeyPressed(LogicalKeyboardKey.arrowRight)) {
+          moveRight();
+        }
+      },
       child: GestureDetector(
         onTap: startGame,
         child: Scaffold(
@@ -45,12 +64,12 @@ class _HomePageState extends State<HomePage> {
                 CoverScreen(
                   gameStarted: gameStarted,
                 ),
-                // 
-                 MyBall(
+                // ball for test
+                MyBall(
                   ballX: ballX,
                   ballY: ballY,
                 ),
-    
+
                 // player
                 MyPlayer(
                   playerX: playerX,
